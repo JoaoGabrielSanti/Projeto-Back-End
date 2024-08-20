d6377a659cbb0934dc26d761b71d1bd72f92d288
Gabriel <jgsgabrielsantiago@gmail.com/>
2024-08-19 22:27:22 -0300

    primeiro commit

---
+++ src/models/imagem.js
@@ -0,0 +1,36 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database.js')
+const Produtos = require('./produtos.js')
+
+const imagens = sequelize.define('imagens', {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+    product_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false,
+        references: {
+            model: Produtos,
+            key: 'id'
+          },
+    },
+    enabled: {
+        type: DataTypes.BOOLEAN,
+        allowNull: true,
+        defaultValue: true
+    },
+    path: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+   
+}, {
+    timestamps: true 
+});
+
+Produtos.hasMany(imagens, { foreignKey: 'product_id' });
+imagens.belongsTo(Produtos);
+
+module.exports = imagens
---
+++ src/services/categoriaService.js
@@ -0,0 +1,31 @@
+const model = require('../models/categorias')
+
+const list = async () => {
+   const list = await model.findAll()
+   return list
+}
+
+const listId = async (req) => {
+    const list = await model.findByPk(req)
+    return list
+}
+
+const create = async (req) => {
+    await model.create(req)
+}
+
+const deletar = async (req) => {
+    await model.destroy({where: {id: req}})
+}
+
+const update = async (id, data) => {
+    await model.update(data, {where: {id: id}})
+}
+
+module.exports = {
+    list,
+    listId,
+    create,
+    deletar,
+    update
+}
